953b6088fee3934417d428736efb055ec71c16a6
fescarvalho <fernando_scarvalho@hotmail.com>
2021-06-14 21:30:12 -0300

    navegação

---
+++ js/components/scroll.js
@@ -0,0 +1,12 @@
+$('nav a').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr("href"),
+        targetOffset = $(id).offset().top;
+
+    
+    $('html, body').animate({
+        scrollTop: targetOffset - 100
+    }, 500);
+
+    
+})
